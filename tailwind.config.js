4ce7cfab82bf1706af39ec0471382f5c5ab16c76
Ashkan-Bah <157798586+Ashkan-Bah@users.noreply.github.com>
2024-04-20 22:12:41 +0330

    a bout me

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend:{
+      colors:{
+          white:"#FFFFFF",
+          white2:"F4F7FA",
+          gray:"#A8A8A8",
+          ornge:"#FF9142",
+          button:"rgba(3,3,4, 50)",
+          background: "#1a1a1a",
+  
+      },
+    },
+    
+    
+      
+      
+
+
+    fontFamily:{
+      Rubik:['Rubik','san-serif'],
+      Syne:['Syne','san-sewif'],
+    }
+    
+  },
+  plugins: [],
+}
+
